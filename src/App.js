20030dc8638fe000d4dfc04dd17ecff6e0db6a04
Nitish Kumar <nitishroy6666@gmail.com>
2022-01-28 23:37:34 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [color, setColor] = useState("1");
+
+  useEffect(() => {
+   clickhandle();
+  });
+  
+  var data ="developer";
+  let data1 = "develop";
+  const data2 = "development";
+
+  function clickhandle(){
+    if (color == "1") {
+      setColor("2");
+    }
+    if (color == "2"){
+    setColor("1");
+    }
+  }
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <div className=''>first react app</div>
+    <h1>hello world</h1>
+    <h1>{data}</h1>
+    <h1>{data1}</h1>
+    <h1>{data2}</h1> */}
+    {/* <button onClick={() => clickhandle()}>click me</button> */}
+
+    <h1>
+      {
+      color=="1"?
+      <h1>red</h1>
+      :
+     
+      <h1>blue</h1>
+      }
+      </h1>
+    </>
   );
 }
 
