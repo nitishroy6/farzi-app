11075693119300dee94b550cb58d52560b3cb55c
Nitish Kumar <nitishroy6666@gmail.com>
2022-02-07 21:58:42 +0530

    api

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -6,7 +6,7 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../style/Header.css';
 import Header1 from '../components/Header1.js';
-
+var sInterval;
 function Home() {
    const [runs, setRuns] = useState("154");
    const [wickets, setWickets] = useState("3");
@@ -27,6 +27,44 @@ function Home() {
 //     setColor("1");
 //     }
 //   }
+// useEffect(()=> {
+//   fetchScore();
+//   },[]);
+
+  useEffect(()=> {
+   sInterval =  setInterval(() => {
+      fetchScore();
+     }, 5000);
+   },[]);
+const fetchScore = async () => {
+let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJudW1iZXJfZXhpc3QiOjEsInN0YXR1cyI6MjAwLCJhYmMiOiI3NjU0MzA0OTQ4IiwibWVzc2FnZSI6IkxvZ2dlZGluIFN1Y2Nlc3NmdWxseSIsInZhbGlkaXR5Ijp0cnVlLCJudW1iZXIiOm51bGx9.pp1Thx6rDVYr-YnkOeOtyDLFvrBDfrApzBF3OtekaIE"; 
+
+const response = await fetch ('https://farziapp.noobdeveloper.site/api/getdata',{
+
+method:'GET',
+
+headers:{
+  'Accept' : 'application/json',
+  'Content-Type' : 'application/json'
+},
+
+ });
+ let retrieved = await response.json();
+ console.log("cateq",retrieved);
+   let totalRun = [];
+      retrieved.map(item => {
+  
+        // totalRun.push({
+        //       label: item.subcate_title,
+        //       value: item.subcate_id
+        //   })
+      });
+
+// const categoryList = await retrieved.info;
+// // console.log("category",categoryList);
+// setCategory(categoryList);
+// console.log("cate",newCategory);
+}
 
   return (
     <div className='body'>
